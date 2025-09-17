34444de41f63fc134f6dd3b301c5b9bc1f2f930b
dschetinin <lut1y@mail.ru>
2025-09-18 01:16:38 +0300

    456

---
+++ staticfiles/js/scripts.js
@@ -0,0 +1,25 @@
+// Основной JavaScript файл
+document.addEventListener('DOMContentLoaded', function() {
+    // Плавная прокрутка для якорных ссылок
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+    
+    // Автоскрытие алертов
+    setTimeout(function() {
+        const alerts = document.querySelectorAll('.alert');
+        alerts.forEach(function(alert) {
+            const bsAlert = new bootstrap.Alert(alert);
+            bsAlert.close();
+        });
+    }, 5000);
+});
